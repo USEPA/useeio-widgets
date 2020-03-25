375d586fb97cd81a16e5c2d3537d83e549907dfa
msrocka <michael.srocka@gmail.com>
2020-03-25 13:29:21 +0100

    responsive SVG

--- src/impact-chart.ts
+++ src/impact-chart.ts
@@ -1,5 +1,32 @@
 import * as d3 from "d3";
 
 export function on(divID: string) {
-    d3.select(`#${divID}`).append("svg");
+
+    // responsive SVG: https://stackoverflow.com/a/25978286
+    const root = d3.select(`#${divID}`)
+        .append("div")
+        .style("display", "inline-block")
+        .style("position", "relative")
+        .style("width", "100%")
+        .style("padding-bottom", "100%")
+        .style("vertical-aling", "top")
+        .style("overflow", "hidden")
+        .append("svg")
+        .attr("preserveAspectRatio", "xMinYMin meet")
+        .attr("viewBox", "0 0 800 800")
+        .style("display", "inline-block")
+        .style("position", "absolute")
+        .style("top", "10px")
+        .style("left", 0)
+        // Class to make it responsive.
+        // .classed("svg-content-responsive", true)
+        // Fill with a rectangle for visualization.
+        .append("rect")
+        .attr("width", 800)
+        .attr("height", 800)
+        .style("fill", "gold")
+        .style("stroke", "steelblue")
+        .style("stroke-width", "5px");
+        
+    // d3.select().append("svg");
 }
