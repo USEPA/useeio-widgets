b7cdf9876cd39bec1bf143d6e68e6057bec15e33
msrocka <michael.srocka@gmail.com>
2020-04-03 19:49:12 +0200

    styling

--- src/sector-list.ts
+++ src/sector-list.ts
@@ -1,6 +1,7 @@
 import * as d3 from "d3";
 import { Sector, WebApi } from "./webapi";
 import { BaseType } from "d3";
+import * as colors from "./colors";
 
 
 interface ListConfig {
@@ -21,7 +22,7 @@ export class SectorList {
     private webapi: WebApi;
     private config: ListConfig;
     private sectors: Sector[] = [];
-    private selection: string[] = [];
+    private selection: Sector[] = [];
     private root: d3.Selection<BaseType, any, HTMLElement, any>;
 
     constructor(config: ListConfig) {
@@ -32,13 +33,13 @@ export class SectorList {
 
     async init() {
         this.sectors = await this.webapi.get("/sectors");
-        this.sectors.sort((s1, s2) => s1.name.localeCompare(s2.name));
         this.root = d3.select(this.config.selector)
             .append("div");
         this.render();
     }
 
     private render() {
+        this.sectors.sort((s1, s2) => this.compare(s1, s2));
         this.root.selectAll("*")
             .remove();
 
@@ -47,6 +48,25 @@ export class SectorList {
             .data(this.sectors)
             .enter()
             .append("div")
+            .style("margin", "1px")
+            .style("padding", "1px")
+            .style("border", (s) => {
+                const idx = this.selection.indexOf(s);
+                if (idx < 0) {
+                    return "none";
+                } else {
+                    const color = colors.toCSS(colors.getChartColor(idx));
+                    return `${color} solid 1px`;
+                }
+            })
+            .style("background-color", (s) => {
+                const idx = this.selection.indexOf(s);
+                if (idx < 0) {
+                    return "white";
+                } else {
+                    return colors.toCSS(colors.getChartColor(idx), 0.1);
+                }
+            })
 
         divs.append("input")
             .attr("type", "checkbox")
@@ -63,21 +83,37 @@ export class SectorList {
         if (!s || !s.code) {
             return false;
         }
-        return this.selection.indexOf(s.code) >= 0;
+        return this.selection.indexOf(s) >= 0;
     }
 
     private selectionChanged(s: Sector) {
         if (!s || !s.code) {
             return;
         }
-        const idx = this.selection.indexOf(s.code);
+        const idx = this.selection.indexOf(s);
         if (idx < 0) {
-            this.selection.push(s.code);
+            this.selection.push(s);
         } else {
             this.selection.splice(idx, 1);
         }
+        this.selection.sort(
+            (s1, s2) => s1.name && s2.name
+                ? s1.name.localeCompare(s2.name)
+                : 0);
         this.render();
         // TODO: fire events...
+    }
+
+    private compare(s1: Sector, s2: Sector): number {
+        const s1Selected = this.isSelected(s1);
+        const s2Selected = this.isSelected(s2);
+        if (s1Selected === s2Selected) {
+            if (!s1.name || !s2.name) {
+                return 0;
+            }
+            return s1.name.localeCompare(s2.name);
+        }
+        return s1Selected ? -1 : 1;
     }
 
 }
