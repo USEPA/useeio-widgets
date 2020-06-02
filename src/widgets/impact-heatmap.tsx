3c1420f73009c8b5bcdb0a2f1de13d0992e586b7
msrocka <michael.srocka@gmail.com>
2020-06-02 11:18:11 +0200

    or better, make it clear in the markup ...
    that the demand is displayed when `showvalues=true`

--- src/widgets/impact-heatmap.tsx
+++ src/widgets/impact-heatmap.tsx
@@ -342,7 +342,7 @@ const Row = (props: RowProps) => {
                     </a>
                 </div>
             </td>
-            {demand ? demand : <></>}
+            {config.showvalues ? demand : <></>}
             <IndicatorResult {...props} />
         </tr>
     );
