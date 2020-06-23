d8cfb8f7ddf3cab3007acef136b6bab10e57cdd9
msrocka <michael.srocka@gmail.com>
2020-06-23 15:51:35 +0200

    adding configuration scopes

--- src/widget.ts
+++ src/widget.ts
@@ -21,6 +21,13 @@ export interface Config {
     sectors?: string[];
 
     /**
+     * A possible additional array of NAICS sector codes.
+     * We translate them to BEA codes when parsing the
+     * confiration.
+     */
+    naics?: string[];
+
+    /**
      * An array of indicator codes.
      */
     indicators?: string[];
@@ -59,23 +66,28 @@ export interface Config {
     page?: number;
 
     /**
-     * `view=mosaic` is currently used in the heatmap
-     * widget to switch between a plain sector list or
-     * the real heatmap.
+     * `view=mosaic` is currently used in the heatmap widget
+     * to switch between a plain sector list or the real heatmap.
      */
     view?: string;
 
     /**
-     * Indicates if result values should be shown in
-     * a widget.
+     * Indicates if result values should be shown in a widget.
      */
     showvalues?: boolean;
 
     /**
-     * Indicates whether code examples should be
-     * displayed.
+     * Indicates whether code examples should be displayed.
      */
     showcode?: boolean;
+
+    /**
+     * Optional scoped configurations. Widgets can be combined
+     * in scopes. Each scope has then its own configuration
+     * state. A scope has a unique name which is added as a
+     * prefix to the configuration options in the URL parameters.
+     */
+    scopes?: { [scope: string]: Config };
 }
 
 export interface WidgetArgs {
@@ -153,18 +165,48 @@ export class UrlConfigTransmitter implements ConfigTransmitter {
      * properties that are not already defined. Only if there is at least one
      * such property, an update is fired.
      */
-    public updateIfAbsent(conf: Record<string, any>) {
+    public updateIfAbsent(conf: Config) {
         if (!conf) return;
-        const next: Record<string, any> = { ...this.config };
-        let needsUpdate = false;
-        for (const key of Object.keys(conf)) {
-            if (!next[key]) {
-                next[key] = conf[key];
-                needsUpdate = true;
+        const next: Config = { ...this.config };
+
+        // set the values of c2 in c1 if they are missing in c1
+        const sync = (c1: Config, c2: Config) => {
+            let needsUpdate = false;
+            for (const key of Object.keys(c2)) {
+                if (key === "scopes") continue;
+                if (!c1[key]) {
+                    c1[key] = c2[key];
+                    needsUpdate = true;
+                }
             }
-        }
-        if (needsUpdate) {
-            this.update(next as Config);
+            if (!c2.scopes) {
+                return needsUpdate;
+            }
+            if (!c1.scopes) {
+                c1.scopes = { ...c2.scopes };
+                return true;
+            }
+            // sync scope configs recursively
+            for (const scope of Object.keys(c2.scopes)) {
+                const cc1 = c1.scopes[scope];
+                const cc2 = c2.scopes[scope];
+                if (!cc2) {
+                    continue;
+                }
+                if (!cc1) {
+                    c1.scopes[scope] = { ...cc2 };
+                    needsUpdate = true;
+                    continue;
+                }
+                if (sync(cc1, cc2)) {
+                    needsUpdate = true;
+                }
+            }
+            return needsUpdate;
+        };
+
+        if (sync(next, conf)) {
+            this.update(next);
         }
     }
 
@@ -183,36 +225,63 @@ export class UrlConfigTransmitter implements ConfigTransmitter {
         this.widgets.push(widget);
         widget.update(this.config);
         widget.onChanged((config) => {
-            this.config = {
-                ...this.config,
-                ...config,
-            };
-            this.updateHash();
+            this.update(config);
         });
     }
 
     update(config: Config) {
-        this.config = {
-            ...this.config,
-            ...config,
-        };
+        const next: Config = { ...this.config };
+        for (const key of Object.keys(config)) {
+            if (key === "scopes") {
+                continue;
+            }
+            next[key] = config[key];
+        }
+
+        // update scopes
+        if (config.scopes) {
+            if (!next.scopes) {
+                next.scopes = { ...config.scopes };
+            } else {
+                for (const scope of Object.keys(config.scopes)) {
+                    const confScope = config.scopes[scope];
+                    const nextScope = next.scopes[scope];
+                    if (!confScope) {
+                        continue;
+                    }
+                    if (!nextScope) {
+                        next.scopes[scope] = {...confScope};
+                        continue;
+                    }
+                    for (const key of Object.keys(confScope)) {
+                        nextScope[key] = confScope[key];
+                    }
+                }
+            }
+        }
+        this.config = next;
         this.updateHash();
     }
 
     private updateHash() {
         const parts = new Array<string>();
-        const conf = this.config;
+        
+        
         if (conf.sectors && conf.sectors.length > 0) {
             parts.push("sectors=" + conf.sectors.join(","));
         }
         if (conf.indicators && conf.indicators.length > 0) {
             parts.push("indicators=" + conf.indicators.join(","));
         }
+        
         for (const key in conf) {
             if (!conf.hasOwnProperty(key)) {
                 continue;
             }
-            if (key === "sectors" || key === "indicators") {
+            if (key === "sectors" 
+                || key === "indicators"
+                || key === "naics"
+                || key === "scopes") {
                 continue;
             }
             const val = conf[key];
@@ -220,6 +289,7 @@ export class UrlConfigTransmitter implements ConfigTransmitter {
                 parts.push(`${key}=${val}`);
             }
         }
+
         window.location.hash = "#" + parts.join("&");
     }
 }
@@ -322,11 +392,11 @@ function updateConfig(config: Config, urlParams: [string, string][]) {
             case "showvalues":
                 config.showvalues = strings.eq(val, "true", "1", "yes");
                 break;
-            
+
             case "showcode":
                 config.showcode = strings.eq(val, "true", "1", "yes");
                 break;
-            
+
             default:
                 break;
         }
